760853ac83cbb62a5f8993a94426b15958247523
Daniel Shahnis <theshahnis@gmail.com>
2024-06-13 00:59:10 +0300

    added profile picture crop

--- backend/app/routes/profile.py
+++ backend/app/routes/profile.py
@@ -5,7 +5,6 @@ from ..forms import UpdateProfileForm
 from ..extensions import db
 import secrets,os
 from PIL import Image
-import logging
 
 profile_bp = Blueprint('profile_bp', __name__)
 
@@ -37,13 +36,7 @@ def profile():
 def save_picture(form_picture, target_dir):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
-    f_ext = f_ext.lower()  # Ensure the extension is in lower case
-
-    if f_ext in ['.jpeg', '.jpg']:
-        picture_fn = f"{random_hex}.jpg"  # Convert JPEG to JPG
-    else:
-        picture_fn = f"{random_hex}{f_ext}"  # Keep original extension for other formats
-
+    picture_fn = random_hex + f_ext
     picture_path = os.path.join(current_app.root_path, target_dir, picture_fn)
 
     if not os.path.exists(os.path.join(current_app.root_path, target_dir)):
@@ -53,15 +46,8 @@ def save_picture(form_picture, target_dir):
         with Image.open(form_picture) as img:
             img.verify()
             form_picture.seek(0)
-            img = Image.open(form_picture)
-
-            if f_ext in ['.jpeg', '.jpg']:
-                img = img.convert('RGB')  # Ensure image is in RGB format
-                img.save(picture_path, 'JPEG')  # Save as JPEG
-            else:
-                img.save(picture_path)  # Save in original format
-
     except (IOError, SyntaxError) as e:
         raise ValueError("Invalid image file")
 
+    form_picture.save(picture_path)
     return picture_fn
