b817f4c203da122969ddb86956bf3ed8272b269e
Daniel Shahnis <theshahnis@gmail.com>
2024-06-13 20:02:07 +0300

    fixed dark mode

--- backend/app/static/service-worker.js
+++ backend/app/static/service-worker.js
@@ -1,40 +1,29 @@
+const CACHE_NAME = 'shlaiman-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/styles.css',
+  '/static/dark-mode.css',
+  '/static/images/favicon.ico',
+  '/static/images/logo1.png',
+  '/static/images/background.png',
+  '/static/images/background2.png',
+  '/static/images/background3.png',
+  '/static/images/background4.png'
+];
+
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('shlaiman-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/static/styles.css',
-          '/static/dark-mode.css',
-          '/static/images/favicon.ico',
-          '/static/images/logo1.png',
-          '/static/images/background.png',
-          '/static/images/background2.png',
-          '/static/images/background3.png',
-          '/static/images/background4.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          console.log('Opened cache');
-          return cache.addAll(urlsToCache);
-        })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          if (response) {
-            return response;
-          }
-          return fetch(event.request);
-        }
-      )
-    );
-  });
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
