fbc118752919addf94af0dc4bfcf81f5efedefb9
Daniel Shahnis <theshahnis@gmail.com>
2024-06-13 14:58:28 +0300

    fixing reset password

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -24,12 +24,12 @@ def create_app():
     app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static/profile_pics')
     #app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
 
-    # Flask-Mail configuration
-    app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-    app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+    app.config['MAIL_SERVER'] = 'localhost'
+    app.config['MAIL_PORT'] = 25
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USERNAME'] = None
+    app.config['MAIL_PASSWORD'] = None
+    app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
     app.config['SECURITY_PASSWORD_SALT'] = os.getenv('SECURITY_PASSWORD_SALT')
 
     db.init_app(app)
@@ -59,7 +59,7 @@ def create_app():
         app.logger.addHandler(file_handler)
         app.logger.setLevel(logging.INFO)
         app.logger.info('ShlaimanFinder startup')
-        
+
     return app
 
 @login_manager.user_loader
