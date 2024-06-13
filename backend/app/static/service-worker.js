4766a361b0e8df836ae83addff671551625a6329
Daniel Shahnis <theshahnis@gmail.com>
2024-06-13 19:52:59 +0300

    fixed dark mode

--- backend/app/static/service-worker.js
+++ backend/app/static/service-worker.js
@@ -11,17 +11,30 @@ self.addEventListener('install', function(event) {
           '/static/images/background2.png',
           '/static/images/background3.png',
           '/static/images/background4.png'
-          // Add other assets you want to cache here
         ]);
       })
     );
   });
   
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
+  self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
     );
   });
   
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
