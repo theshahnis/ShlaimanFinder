d057b38e748a41b9cf47dbe7d2f393614c421c6f
Daniel Shahnis <theshahnis@gmail.com>
2024-06-15 23:06:22 +0300

    added json logic for auth login

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -7,7 +7,7 @@ from itsdangerous import URLSafeTimedSerializer
 from flask_mail import Message
 from ..forms import RequestResetForm
 import smtplib, jwt
-
+from functools import wraps
 
 
 auth_bp = Blueprint('auth_bp', __name__)
