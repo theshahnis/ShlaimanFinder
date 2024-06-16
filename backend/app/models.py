4727331a5e7ef563c84132ee108b40b60df5ef02
Daniel Shahnis <theshahnis@gmail.com>
2024-06-16 20:23:41 +0300

    Added jwt logic

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,7 @@
 from .extensions import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
+from datetime import datetime,timedelta
 import pytz
 
 class Group(db.Model):
