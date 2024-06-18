3e043ed42b533bcf8559c2a14ed5b8287ffd4bc1
Daniel Shahnis <theshahnis@gmail.com>
2024-06-18 23:33:46 +0300

    added ids to locations

--- backend/app/static/service-worker.js
+++ backend/app/static/service-worker.js
@@ -12,22 +12,63 @@ const urlsToCache = [
   '/static/images/eagle_logo.png',
   '/static/images/hawk_logo.png',
   '/static/images/buzzard_logo.png',
-  '/static/images/vulture_logo.png'
+  '/static/images/vulture_logo.png',
+  '/static/shows.js',
+  '/static/my_shows.js',
+  '/static/map.js',
 ];
 
-self.addEventListener('install', function(event) {
+// Install event - cache specified URLs and skip waiting to activate the new service worker immediately
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
+    caches.open(CACHE_NAME).then(cache => {
       console.log('Opened cache');
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();  // Forces the waiting service worker to become the active service worker
 });
 
-self.addEventListener('fetch', function(event) {
+// Fetch event - serve from cache if available, otherwise fetch from network and cache it
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then(networkResponse => {
+        if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+          return networkResponse;
+        }
+
+        // Clone the response
+        const responseToCache = networkResponse.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          if (event.request.url.includes('/profile_pics/') || event.request.headers.get('accept').includes('text/html')) {
+            cache.put(event.request, responseToCache);
+          }
+        });
+
+        return networkResponse;
+      });
     })
   );
+});
+
+// Activate event - delete old caches and claim clients immediately
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();  // Ensures that the service worker takes control of all clients as soon as it activates
 });
