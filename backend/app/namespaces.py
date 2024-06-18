ea5198d7d3e1d0b864bc7c4af62f1b293673931d
Daniel Shahnis <theshahnis@gmail.com>
2024-06-18 16:47:28 +0300

    added api docs

--- backend/app/namespaces.py
+++ backend/app/namespaces.py
@@ -1,5 +1,5 @@
-from routes.api import api as api_namespace
-from routes.profile import profile_ns
+from .routes.api import api as api_namespace
+from .routes.profile import profile_ns
 
 def register_namespaces(api):
     api.add_namespace(api_namespace, path='/api/v1')
