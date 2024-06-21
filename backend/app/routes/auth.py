dba3227ff865db2820245b58e88dede1ac5425db
Daniel Shahnis <theshahnis@gmail.com>
2024-06-21 19:37:46 +0300

    fixed bug in logout

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash, current_app
+from flask import Blueprint, render_template, redirect, url_for, request, flash, current_app,session
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from ..models import User
