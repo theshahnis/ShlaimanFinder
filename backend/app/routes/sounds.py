66abac7ca3742d8500d9cd012441a674ae123f99
Daniel Shahnis <theshahnis@gmail.com>
2024-06-21 21:12:51 +0300

    added sound function

--- backend/app/routes/sounds.py
+++ backend/app/routes/sounds.py
@@ -13,27 +13,6 @@ ALLOWED_EXTENSIONS = {'wav', 'mp3', 'ogg'}
 def sounds_page():
     return render_template('sound.html')
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@sounds_bp.route('/upload', methods=['POST'])
-@token_or_login_required
-def upload_sound():
-    if 'file' not in request.files:
-        return jsonify({'message': 'No file part'}), 400
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'message': 'No selected file'}), 400
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        filepath = os.path.join(current_app.root_path, SOUND_UPLOAD_FOLDER, filename)
-        try:
-            file.save(filepath)
-            return jsonify({'message': 'File uploaded successfully', 'filename': filename}), 200
-        except Exception as e:
-            return jsonify({'message': 'File upload failed', 'error': str(e)}), 500
-    return jsonify({'message': 'Invalid file type'}), 400
-
 @sounds_bp.route('/get_sounds', methods=['GET'])
 @token_or_login_required
 def list_sounds():
@@ -43,7 +22,7 @@ def list_sounds():
     except Exception as e:
         return jsonify({'message': 'Failed to list sounds', 'error': str(e)}), 500
 
-@sounds_bp.route('/sounds/<filename>', methods=['GET'])
+@sounds_bp.route('/<filename>', methods=['GET'])
 @token_or_login_required
 def get_sound(filename):
     try:
