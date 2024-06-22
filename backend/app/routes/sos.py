1b50535633da29ed8144996e3bcdafea267c7385
Daniel Shahnis <theshahnis@gmail.com>
2024-06-22 22:50:10 +0300

    added sos function

--- backend/app/routes/sos.py
+++ backend/app/routes/sos.py
@@ -3,8 +3,8 @@ from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required, current_user
 from app.models import User
 from app.extensions import db
-import requests
 import os
+from twilio.rest import Client
 from .api import token_or_login_required
 
 sos_bp = Blueprint('sos_bp', __name__)
@@ -18,52 +18,56 @@ def sos_page():
 @token_or_login_required
 def send_sos():
     data = request.get_json()
-    current_location = data.get('location', 'Location not provided')
+    latitude = data.get('latitude', None)
+    longitude = data.get('longitude', None)
+    if not latitude or not longitude:
+        return jsonify({'message': 'Coordinates not provided'}), 400
+
+    google_maps_link = f"https://www.google.com/maps/search/?api=1&query={latitude},{longitude}"
+    current_location = google_maps_link
 
     group_id = current_user.group_id
     if not group_id:
         return jsonify({'message': 'You are not part of any group'}), 400
 
     group_members = User.query.filter_by(group_id=group_id).all()
-    # phone_numbers = [member.phone_number for member in group_members if member.phone_number]
-    phone_numbers = ['+972547537103']
+    phone_numbers = [member.phone_number for member in group_members if member.phone_number]
+    # phone_numbers = ["+972547537103"]  # For testing purposes
 
-    message = f"SOS - Need Assistance\nInitiator: {current_user.username}\nLocation: {current_location}"
+    message_template_name = 'sos_alert'
+    initiator_username = current_user.username
 
     for number in phone_numbers:
-        print(f"trying to send message to {number}")
-        send_sms_or_whatsapp(number, message)
+        send_sms_or_whatsapp(number, message_template_name, initiator_username, current_location)
 
-    return jsonify({'message': f'SOS message sent to group members by {current_user.username} to {phone_numbers}'}), 200
+    return jsonify({'message': 'SOS message sent to group members'}), 200
 
-def send_sms_or_whatsapp(phone_number, message):
+def send_sms_or_whatsapp(phone_number, template_name, initiator_username, location):
     try:
-        account_sid = os.getenv('ACCOUNT_SID')
+        account_sid = os.getenv('TWILIO_ACCOUNT_SID')
         auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+        client = Client(account_sid, auth_token)
+
         from_whatsapp_number = 'whatsapp:+12518108458'
         to_whatsapp_number = f'whatsapp:{phone_number}'
-        body = message
 
-        # Logging the environment variables and request data
-        print(f"Account SID: {account_sid}")
-        print(f"Auth Token: {auth_token}")
-        print(f"From: {from_whatsapp_number}")
-        print(f"To: {to_whatsapp_number}")
-        print(f"Body: {body}")
+        body_params = [initiator_username, location]
 
-        url = f'https://api.twilio.com/2010-04-01/Accounts/{account_sid}/Messages.json'
-        data = {
-            'From': from_whatsapp_number,
-            'To': to_whatsapp_number,
-            'Body': body
-        }
-        response = requests.post(url, data=data, auth=(account_sid, auth_token))
+        message = client.messages.create(
+            from_=from_whatsapp_number,
+            to=to_whatsapp_number,
+            provide_feedback=True,
+            persistent_action=[
+                {
+                    "type": "template",
+                    "template_name": template_name,
+                    "template_language": "en",  # Adjust language code if necessary
+                    "body_params": body_params
+                }
+            ]
+        )
 
-        # Logging the response
-        print(f"Twilio Response Status: {response.status_code}")
-        print(f"Twilio Response Text: {response.text}")
+        print(f"Message SID: {message.sid}")
 
-        if response.status_code != 201:
-            print(f"Failed to send message to {phone_number}: {response.text}")
     except Exception as e:
         print(f"Error sending message using Twilio: {e}")
