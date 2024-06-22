075d68ba7f15a69be0ee06aa1fd40fbe05aa8a37
Daniel Shahnis <theshahnis@gmail.com>
2024-06-22 21:53:06 +0300

    added sos function

--- backend/app/routes/sos.py
+++ backend/app/routes/sos.py
@@ -3,9 +3,9 @@ from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required, current_user
 from app.models import User
 from app.extensions import db
-import requests,os
+import requests
+import os
 from .api import token_or_login_required
-
 
 sos_bp = Blueprint('sos_bp', __name__)
 
@@ -25,8 +25,8 @@ def send_sos():
         return jsonify({'message': 'You are not part of any group'}), 400
 
     group_members = User.query.filter_by(group_id=group_id).all()
-    #phone_numbers = [member.phone_number for member in group_members if member.phone_number]
-    phone_numbers= ["+972547537103"]
+    # phone_numbers = [member.phone_number for member in group_members if member.phone_number]
+    phone_numbers = ["+972547537103"]
 
     message = f"SOS - Need Assistance\nInitiator: {current_user.username}\nLocation: {current_location}"
 
@@ -36,14 +36,19 @@ def send_sos():
     return jsonify({'message': 'SOS message sent to group members'}), 200
 
 def send_sms_or_whatsapp(phone_number, message):
-    # Implement SMS/Whatsapp API call here
-    # Example using Twilio (replace with your API details):
     try:
         account_sid = os.getenv('ACCOUNT_SID')
         auth_token = os.getenv('TWILIO_AUTH_TOKEN')
         from_whatsapp_number = 'whatsapp:+12518108458'
         to_whatsapp_number = f'whatsapp:{phone_number}'
         body = message
+
+        # Logging the environment variables and request data
+        print(f"Account SID: {account_sid}")
+        print(f"Auth Token: {auth_token}")
+        print(f"From: {from_whatsapp_number}")
+        print(f"To: {to_whatsapp_number}")
+        print(f"Body: {body}")
 
         url = f'https://api.twilio.com/2010-04-01/Accounts/{account_sid}/Messages.json'
         data = {
@@ -53,7 +58,11 @@ def send_sms_or_whatsapp(phone_number, message):
         }
         response = requests.post(url, data=data, auth=(account_sid, auth_token))
 
+        # Logging the response
+        print(f"Twilio Response Status: {response.status_code}")
+        print(f"Twilio Response Text: {response.text}")
+
         if response.status_code != 201:
             print(f"Failed to send message to {phone_number}: {response.text}")
     except Exception as e:
-        print(f"Error sending message using twilio:{e}")
+        print(f"Error sending message using Twilio: {e}")
